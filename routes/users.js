db18e70e6ded75824b9e8ae3ad7928a0a82640cb
rizkydar3007 <kakarizky3007@gmail.com>
2022-09-29 12:14:55 +0700

    expressJS

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send(process.env.APP_NAME);
+});
+router.get("/me", (req, res, next) => {
+  res.send("Hi, this me!");
+});
+
+module.exports = router;
